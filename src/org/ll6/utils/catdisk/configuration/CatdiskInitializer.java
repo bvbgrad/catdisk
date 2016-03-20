2c91fa9fc0b5cf8c7d3a05b575abb3788f414d29
Brent Bingham <bvbgrad@gmail.com>
2016-03-20 16:23:45 -0500

    added log4j

--- src/org/ll6/utils/catdisk/configuration/CatdiskInitializer.java
+++ src/org/ll6/utils/catdisk/configuration/CatdiskInitializer.java
@@ -1,16 +1,16 @@
 package org.ll6.utils.catdisk.configuration;
 
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class CatdiskInitializer extends 
 	AbstractAnnotationConfigDispatcherServletInitializer {
-//	private static final Logger logger = LogManager.getLogger();
+	private static final Logger logger = LogManager.getLogger();
  
     @Override
     protected Class<?>[] getRootConfigClasses() {
-//		logger.info("Initializing configuration classes");
+		logger.info("Initializing configuration classes");
         return new Class[] { CatdiskConfiguration.class };
     }
   
