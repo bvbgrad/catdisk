87d6e4baa97f0c15cd2603d98dc02c3e58590628
Brent V. Bingham <bvbgrad@gmail.com>
2016-07-10 05:25:41 -0500

    Format date just before display, add edit button (no dynamic diskID)

--- php/src/AppBundle/Controller/DiskFileController.php
+++ php/src/AppBundle/Controller/DiskFileController.php
@@ -23,13 +23,13 @@ class DiskFileController extends Controller
 		$json = file_get_contents("http://localhost:8080/catdisk/getDiskCount");
 		$diskCount = json_decode($json, TRUE);
 		
-		$defaultData = array('start' => '1', 'stop' => '0', 'page' => '25');
+		$defaultData = array('start' => '1', 'end' => '0', 'page' => '25');
 		$form = $this->createFormBuilder($defaultData)
 	        ->add('start', IntegerType::class, [
 	    			'label' => 'Disk start number: '
 	    	])
-	        ->add('stop', IntegerType::class, [
-	    			'label' => 'Disk stop number: '
+	        ->add('end', IntegerType::class, [
+	    			'label' => 'Disk end number: '
 	    	])
 // 	        ->add('page', IntegerType::class, [
 // 	    			'label' => 'Number per page: ',
@@ -50,14 +50,14 @@ class DiskFileController extends Controller
 	        $data = $form->getData();
 	        
 	        $start = $data['start'];
-	        $stop = $data['stop'];
+	        $end = $data['end'];
 	        
-	        if ($stop == 0)
+	        if ($end == 0)
 	        {
 		        return $this->redirectToRoute("disk", array('diskID' => $start));
 	        } else
 	        {
-		        return $this->redirectToRoute("diskrange", array('start' => $start, 'stop' => $stop));
+		        return $this->redirectToRoute("diskrange", array('start' => $start, 'end' => $end));
 	        }
 	        
 	        
@@ -71,33 +71,50 @@ class DiskFileController extends Controller
 	}
 
 	/**
-	 * @Route("/diskrange/{start}/{stop}", name="diskrange",
-	 * 		defaults={"start" = 1, "stop" = 10}
+	 * @Route("/diskrange/{start}/{end}", name="diskrange",
+	 * 		defaults={"start" = 1, "end" = 10}
 	 * )
 	 */
-	public function formDiskListRangeAction(Request $request, $start, $stop)
+	public function formDiskListRangeAction(Request $request, $start, $end)
 	{
 		$json = file_get_contents("http://localhost:8080/catdisk/getDiskCount");
 		$diskCount = json_decode($json, TRUE);
 		
 		$json = file_get_contents
-	    	("http://localhost:8080/catdisk/getDiskRange/".$start."/".$stop);
+	    	("http://localhost:8080/catdisk/getDiskRange/".$start."/".$end);
 	    $diskData = json_decode($json, TRUE);
 	    if (json_last_error() === JSON_ERROR_NONE) {
 	    	//do something with $json. It's ready to use
 		    dump($diskData);
+		    foreach ($diskData as &$a) 
+		    {
+		    	$a['diskDate'] = date("Y-m-d", $a['diskDate']/1000);
+		    	$a['createdOn'] = date("Y-m-d", $a['createdOn']/1000);
+		    	$a['modifiedOn'] = date("Y-m-d", $a['modifiedOn']/1000);
+		    };
+	    	unset($a);
+	    	dump ($diskData);
+	
+			return $this->render('diskfile/diskrange.html.twig', array(
+				'diskCount' => $diskCount,
+				'start' => $start,
+				'end' => $end,
+				'count' => $end - $start + 1,
+				'disks' => $diskData
+	        ));		
 	    } else {
 	    	//yep, it's not JSON. Log error or alert someone or do nothing
 		    dump(json_last_error());
+		    dump(json_last_error_msg());
+		    
+		    $msg = "<script> alert('json error: '.json_last_error_msg()) </script>";
+		    echo $msg;
+		    
+		    return $this->render('menu/home.html.twig', array(
+		    		'diskCount' => $diskCount
+		    ));
 	    }
-
-		return $this->render('diskfile/diskrange.html.twig', array(
-			'diskCount' => $diskCount,
-			'start' => $start,
-			'end' => $stop,
-			'count' => $stop - $start +1,
-			'disks' => $diskData
-        ));		
+	    
 	}
 	
 	/**
